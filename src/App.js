e25b73fce50eded7c0d0304ab9f5250acb293ee0
zyryanov_ivan <89224747505@mail.ru>
2022-01-21 16:10:46 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import './App.css';
-import MyPost from "./components/MyPost/MyPost";
 import {useState} from "react";
 import NewPostForm from "./components/NewPostForm/NewPostForm";
 import MyPostList from "./components/MyPostList/MyPostList";
 
 
 const App = () => {
-    const [allPosts, setAllPosts] = useState([
-        {
-            id: 1, title: "Название: Как танцуют попугаи?", content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci velit reiciendis veritatis,
-                quae quas iusto exercitationem officiis, consequatur harum molestias voluptates sapiente! Ullam
-                expedita quis non debitis voluptates corrupti reprehenderit laborum hic blanditiis reiciendis
-                neque eos asperiores aperiam sint officiis a inventore consequuntur natus, ea sequi similique.`
-        }
-    ]);
+    const [allPosts, setAllPosts] = useState([]);
 
     const addPost = (post) => {
         setAllPosts([...allPosts, post]);
 
     }
     const deletePost = (id) => {
-        setAllPosts(allPosts.filter(p=> p.id !== id));
+        setAllPosts(allPosts.filter(p => p.id !== id));
     }
     return (
         <div className="App">
             <NewPostForm addPost={addPost}/>
-            <MyPostList deletePost={deletePost} posts={allPosts}/>
+            {allPosts.length !== 0
+                ? <MyPostList deletePost={deletePost} posts={allPosts}/>
+                : <div className="errorMessage">В этой ветке новых постов не опубликованно</div>}
+
         </div>
     );
 }
--- src/components/MyPostList/MyPostList.jsx
+++ src/components/MyPostList/MyPostList.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import MyPost from "../MyPost/MyPost";
 
 const MyPostList = (props) => {
-    const renderMyPostList = () =>{
-        debugger
-        if (props.posts.length != 0) props.posts.map((p,index) => <MyPost key={p.id} deletePost={props.deletePost} number={index+1} post={p}/>)
-    }
-
     return (
         <div>
-            {renderMyPostList()}
+            {props.posts.map((p,index) => <MyPost key={p.id} deletePost={props.deletePost} number={index+1} post={p}/>)}
         </div>
     );
 };
